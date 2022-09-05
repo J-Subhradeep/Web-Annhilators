931889caa4f6071553071c3ddfa50f26a6de12ef
Pratyush <pratyushbanerjeeadv6@gmail.com>
2022-09-05 14:43:21 +0530

    All the drafts for the beginning

---
+++ Static/Js/Draft files js/IBWcalculator.js
@@ -0,0 +1,24 @@
+function checkgender() {
+    let selected= document.querySelector('input[type="radio"]:checked')
+if(selected.value=='male')
+{
+    alert('male')
+    male();
+}
+else if(selected.value=='fmale')
+{
+    fmale();
+}
+else
+alert('Choose Gender to Get Ideal Body Weight');  
+}
+function male() {
+    var height= parseInt(document.getElementById("tall001").value);
+    var IBW= 22*(height/100)*(height/100);
+document.getElementById('fbmi01').innerHTML=`Your Ideal Body Weight is= ${IBW}`;
+}
+function fmale() {
+    var height= parseInt(document.getElementById("tall001").value);
+    var IBW= 22*((height/100)-0.1)*((height/100)-0.1);
+document.getElementById('fbmi01').innerHTML=`Your Ideal Body Weight is= ${IBW}`;
+}
