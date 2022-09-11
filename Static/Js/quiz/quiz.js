979434f5198d9c27e9d171ba77ad0ee7f2ade5f6
Pratyush <pratyushbanerjeeadv6@gmail.com>
2022-09-11 21:24:35 +0530

    quiz

---
+++ Static/Js/quiz/quiz.js
@@ -0,0 +1,30 @@
+// const setUp=[
+//     {
+//         question: "What is your purpose of doing Gym?",
+//         a: "Lose Weight",
+//         b: "Gain Weight",
+//         c:"Gain Muscle"
+//     },{
+//         question: "How do You Want to look after the results?",
+//         a: "Less Fat",
+//         b: "Less Slim",
+//         c:"Buffed Up"
+//     },{
+//         question: "What Kind of Body type does your significant other Likes?",
+//         a: "Slim",
+//         b: "Chubby",
+//         c:"Buffed Up"
+//     }
+// ];
+// const question1=document.getElementsByClassName('.quesTion');
+// const opTion1= document.getElementById('#anS1');
+// const opTion2= document.getElementById('#anS2');
+// const opTion3= document.getElementById('#anS3');
+// const subBtn= document.getElementsByClassName('.suBmit');
+// console.log(question1);
+// const loadQue =()=>{
+//     // question1.innerHtml=setUp[0].question;
+//     console.log(setUp[0]);
+// }
+// loadQue();
+console.log('ABCD');
